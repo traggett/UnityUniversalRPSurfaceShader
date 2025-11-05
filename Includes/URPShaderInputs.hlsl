cd69baa02db297a883b815c7ebb7c8994cc0b0fd
traggett <traggett@googlemail.com>
2025-11-05 18:45:20 +0000

    Fixed shadow instancing

--- Includes/URPShaderInputs.hlsl
+++ Includes/URPShaderInputs.hlsl
@@ -92,6 +92,7 @@ struct Attributes
     float4 positionOS   : POSITION;
     float3 normalOS     : NORMAL;
     float2 texcoord     : TEXCOORD0;
+	
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
@@ -99,6 +100,9 @@ struct Varyings
 {
 	float2 uv       	: TEXCOORD0;
     float4 positionCS   : SV_POSITION;
+	
+	UNITY_VERTEX_INPUT_INSTANCE_ID
+    UNITY_VERTEX_OUTPUT_STEREO
 };
 
 /////////////////////////////////////////
